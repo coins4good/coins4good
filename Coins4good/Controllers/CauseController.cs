ccc20db765d32ccf8c95c57985bc9aad4e482379
coins4good <coins4good@outlook.com>
2014-04-04 16:07:08 +0200

    First commit
    First commit web version

---
+++ Coins4good/Controllers/CauseController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Coins4good.Controllers
+{
+    public class CauseController : Controller
+    {
+        // GET: Cause
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Coins4good/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Coins4good.MvcApplication" Language="C#" %>
